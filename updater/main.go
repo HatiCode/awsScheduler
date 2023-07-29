c0f94458e7e1f1a2e171a0f11d35907580d902c5
Alexandre Steppé <alexandre.steppe@gmail.com>
2023-07-29 23:25:32 +0200

    test sqs

--- updater/main.go
+++ updater/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/HatiCode/awsScheduler/updater/cmd"
 	"github.com/HatiCode/awsScheduler/updater/utils"
+	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 func main() {
+	fmt.Println("testing S3")
 	cmd.ListS3()
+
+	fmt.Println("Creating SQS")
+	sess := session.Must(session.NewSession())
+	utils.CreateSQS(sess, "test-queue")
 }
